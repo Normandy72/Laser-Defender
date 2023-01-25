87281aae888fe2d19832851f69d88b533ddd4af8
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-01-25 20:25:44 +0100

    add enemy shooting logic

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileLifetime = 5f;
-    [SerializeField] float firingRate = 0.2f;
+    [SerializeField] float baseFiringRate = 0.2f;
 
-    public bool isFiring;
+    [Header("AI")]
+    [SerializeField] bool useAI;
+    [SerializeField] float firingRateVariance = 0f;
+    [SerializeField] float minimumFiringRate = 0.1f;
+
+    [HideInInspector] public bool isFiring;
 
     Coroutine firingCoroutine;
 
     void Start()
     {
-        
+        if(useAI)
+        {
+            isFiring = true;
+        }
     }
 
     void Update()
@@ -49,7 +58,12 @@ public class Shooter : MonoBehaviour
 
             Destroy(instance, projectileLifetime);
 
-            yield return new WaitForSeconds(firingRate);
+            float timeToNextProjectile = Random.Range(baseFiringRate - firingRateVariance, 
+                                                      baseFiringRate + firingRateVariance);
+
+            timeToNextProjectile = Mathf.Clamp(timeToNextProjectile, minimumFiringRate, float.MaxValue);
+
+            yield return new WaitForSeconds(timeToNextProjectile);
         }
     }
 }
