10dbbeddc89488382e611d7beadc5819c5bc5db2
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-01-26 19:14:52 +0100

    add audio effects

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -18,6 +18,12 @@ public class Shooter : MonoBehaviour
     [HideInInspector] public bool isFiring;
 
     Coroutine firingCoroutine;
+    AudioPlayer audioPlayer;
+
+    void Awake()
+    {
+        audioPlayer = FindObjectOfType<AudioPlayer>();
+    }
 
     void Start()
     {
@@ -62,6 +68,8 @@ public class Shooter : MonoBehaviour
                                                       baseFiringRate + firingRateVariance);
 
             timeToNextProjectile = Mathf.Clamp(timeToNextProjectile, minimumFiringRate, float.MaxValue);
+
+            audioPlayer.PlayShootingClip();
 
             yield return new WaitForSeconds(timeToNextProjectile);
         }
